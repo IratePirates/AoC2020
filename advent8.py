c19c3ff80a48704831e5930c7930ccd18aa25014
IratePirateS <steve@scratch-my-belly.com>
2020-12-13 13:33:47 +0000

    [Day8] Fix part 2.
    The intcode object wasn't being reset between runs, so the stale
    program history was jamming up the mechanism to check whether or
    not we had a loop, or ended naturally

--- advent8.py
+++ advent8.py
@@ -11,15 +11,14 @@ def parse_input(in_f):
 
 
 class intcode_again:
-    acc = 0
-    pc = 0
-    history = []
-    instr = []
     _dispatch = {}
 
     def __init__(self, prog):
-        self.instr = copy.deepcopy(prog)
-
+        self.acc = 0
+        self.pc = 0
+        self.prog = copy.deepcopy(prog)
+        self.prog_len = len(self.prog)
+        self. history =  []
         self._dispatch = { 'jmp' : lambda x : self.jump(x),
                            'acc' : lambda x : self.accumulate(x),
                            'nop' : lambda x : self.noop(x),
@@ -35,38 +34,66 @@ class intcode_again:
         self.pc += 1
         self.acc += val 
 
-    def run_instruction(self, instr):
-        if instr[0] not in self.history:
-            print(instr)
-            self._dispatch[instr[1]](instr[2])
-            self.history.append(instr[0])
-            return None
+    def run_instruction(self):
+        if self.pc >= self.prog_len:
+            return (self.acc, False)
         else:
-            return self.acc
+            instr = self.prog[self.pc]
+            if instr[0] in self.history:
+                return (self.acc, True)
+            else:
+                self._dispatch[instr[1]](instr[2])
+                self.history.append(instr[0])
+                return (None, False)
 
     def print_state(self):
         print(f"PC: {self.pc} ACC: {self.acc}, {self.history}")
 
     def run(self):
         print("Starting the new intcode")
-        res = self.run_instruction(self.instr[0])
+        res, looping = self.run_instruction()
         cnt = 0
         while res is None:
-            print(cnt)
-            res = self.run_instruction(self.instr[self.pc])
-            self.print_state()
+            res, looping = self.run_instruction()
+            # self.print_state()
             cnt += 1
-        return(res)
+        return res, looping
  
 
 def part1(in_f):
     instr = parse_input(in_f)
     interp = intcode_again(instr)
-    return interp.run()
+    res, looping = interp.run()
+    return res
 
+
+def part2(in_f):
+    instr = parse_input(in_f)
+    print("Initial Instr:", instr)
+
+    for f in range(len(instr)):
+        new_instr = copy.deepcopy(instr)
+        if new_instr[f][1] == "nop":
+            new_instr[f] = (new_instr[f][0], "jmp", new_instr[f][2])
+        elif new_instr[f][1] == "jmp":
+            new_instr[f] = (new_instr[f][0], "nop", new_instr[f][2])
+        else:
+            continue
+
+        interp = intcode_again(new_instr)
+        res, looping = interp.run()
+
+        if looping == False:
+            return(res)
+    return None
 
 #print(parse_input("input8.txt"))
-#assert(part1("test8.txt" == 5))
+#print(part1("test8.txt"))
+assert(part1("test8.txt") == 5)
 print(part1("input8.txt"))
+assert(part1("input8.txt") == 1749)
+
+assert(part2("test8.txt") == 8)
+print(part2("input8.txt"))
 
 
